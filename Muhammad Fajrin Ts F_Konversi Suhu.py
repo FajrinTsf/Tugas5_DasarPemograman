82e3f0cef0be59b9e9f0c9fc7a4dc7e48bad5f42
pEGBOL/p <147245065+FajrinTsf@users.noreply.github.com>
2024-03-24 20:17:48 +0700

    Add files via upload

---
+++ Muhammad Fajrin Ts F_Konversi Suhu.py
@@ -0,0 +1,12 @@
+print('Halo Selamat Datang {} , Selamat belajar {}'
+      .format('Muhammad Fajrin Tsinan F', 'Dasar Pemograman'))
+# Menampilkan Output Nama dan Matkul
+
+print('Kalkulator Koversi Suhu CelCius ke Farenheit')
+# Kalkulator Konversi Suhu C ke F
+celcius = float(input("Masukan Celcius : "))
+# memasukan Nilai Celcius kedalam variabel
+fahrenheit = (celcius * 9/5) + 32
+# Formula Konversi Celcius ke Farenheit
+print("Suhu dalam Fahrenheit adalah: " + str(fahrenheit) + "F")
+# Menampilkan Hasil Konversi
